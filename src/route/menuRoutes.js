7d4b37127171787187a1353c504cd59bf60cd61f
manhhung <daomanhhung1202@gmail.com>
2025-07-20 15:40:43 +0700

    fix add menu

--- src/route/menuRoutes.js
+++ src/route/menuRoutes.js
@@ -8,4 +8,6 @@ router.post("/", menuController.createMenu);
 router.put("/:id", menuController.updateMenu);
 router.delete("/:id", menuController.deleteMenu);
 
+// step api
+
 module.exports = router;
